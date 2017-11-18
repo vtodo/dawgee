1f5def514accaea882058d12c65542cd1f1c28da
vtodorov@msn.com <vtodorov@msn.com>
2017-11-18 13:04:05 +0200

    initial commit, testing

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import tornado.web
 import logging
 
-log = logging.getLogger("blogTest")
+log = logging.getLogger("AppHandler")
 log.setLevel(logging.DEBUG)
 
-class HelloWorldHandler(tornado.web.RequestHandler):
+class AppHandler(tornado.web.RequestHandler):
     def get(self):
         log.info('Hello ')
         self.render('index.html')
     
 app = tornado.web.Application(handlers=[
-    (r"/", HelloWorldHandler),
+    (r"/", AppHandler),
     (r"/assets/(.*)", tornado.web.StaticFileHandler, {"path": "assets/"})
 ])
