56a557d5e91311c48d54bc04b9f418bcb3fd9977
vtodorov@msn.com <vtodorov@msn.com>
2017-11-18 21:38:55 +0200

    Add uglify to buildchain

--- postinstall.sh
+++ postinstall.sh
@@ -3,11 +3,13 @@ if [ -z "$PRODUCTION" ]; then
     browserify --transform coffeeify --debug coffee/main.coffee > dist/js/main.bundle.js
 else
     echo Building for production &&
-    browserify --transform coffeeify coffee/main.coffee > dist/js/main.bundle.js
+    browserify --transform coffeeify coffee/main.coffee > dist/js/main.pretty.js
 fi
 
 if [ -z "$PRODUCTION" ]; then
     echo Would not uglify
 else
-    echo Would uglify 100%
+    echo Would uglify 100% &&
+    npx uglifyjs -o dist/js/main.bundle.js dist/js/main.pretty.js &&
+    rm dist/js/main.pretty.js
 fi
